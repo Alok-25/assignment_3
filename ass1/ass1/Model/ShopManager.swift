c9677c13f3e695bf8dbf258801328ad2f844670e
Alok <alok.tripathi@inito.com>
2023-08-06 19:37:24 +0530

    store functionality added

---
+++ ass1/ass1/Model/ShopManager.swift
@@ -0,0 +1,12 @@
+//
+//  ShopManager.swift
+//  ass1
+//
+//  Created by Inito on 05/08/23.
+//
+
+import Foundation
+
+struct ShopManager{
+    
+}
---
+++ ass1/ass1/ProductCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductCell.swift
+//  ass1
+//
+//  Created by Inito on 06/08/23.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    @IBOutlet weak var LabelDescription: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
